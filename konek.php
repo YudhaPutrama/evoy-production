bd3e442c27ac69dac1a8391638e6daa8b4c2b6b7
Kurniawan Yudha Putrama <kurniawan.yudha.p@gmail.com>
2017-05-03 21:25:20 +0700

    change database setting

--- konek.php
+++ konek.php
@@ -1,7 +1,8 @@
 <?php
 	$username="evoy";
 	$password="evoy";
-	$dbname="(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=trivalier.id)(PORT=49161))(CONNECT_DATA=(SID=XE)))";
+	// $dbname="(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=trivalier.id)(PORT=49161))(CONNECT_DATA=(SID=XE)))";
+	$dbname = "trivalier.id:49161/XE";
 	$conn=oci_connect($username, $password, $dbname);
 		if (!$conn) {
 			echo "Koneksi ke server database gagal dilakukan";
